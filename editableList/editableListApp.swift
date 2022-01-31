e16b8da14d3a875f2882c073b3b03812839b0624
cfwdman <dev@medienkueche.de>
2022-01-31 21:47:09 +0100

    Initial Commit

---
+++ editableList/editableListApp.swift
@@ -0,0 +1,17 @@
+//
+//  editableListApp.swift
+//  editableList
+//
+//  Created by Christoph Dohrmann on 31.01.22.
+//
+
+import SwiftUI
+
+@main
+struct editableListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
